ffa200b3e2fc8a6a92368f599bfb52b2d481603f
root <root@Kvin2601>
2024-05-07 13:31:57 +0200

    add ( before if line 54 and ) after :

--- debugging/mines.py
+++ debugging/mines.py
@@ -51,7 +51,10 @@ class Minesweeper:
             for dx in [-1, 0, 1]:
                 for dy in [-1, 0, 1]:
                     nx, ny = x + dx, y + dy
-                    if 0 <= nx < self.width and 0 <= ny < self.height and not self.revealed[ny][nx]:
+                    if (
+                        0 <= nx < self.width and 0 <= ny < self.height
+                        and not self.revealed[ny][nx]
+                    ):
                         self.reveal(nx, ny)
         return True
 
